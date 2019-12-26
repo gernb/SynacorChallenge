9526960f903cb6c4b6856ba54ec5ea7cb029e5d2
peter bohac <pbohac@apple.com>
2019-12-25 19:57:24 -0800

    Initial Commit

---
+++ SynacorVM/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SynacorVM
+//
+//  Created by peter bohac on 12/25/19.
+//  Copyright © 2019 peter bohac. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
