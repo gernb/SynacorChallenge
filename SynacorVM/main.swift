311ac4fb37595068cd3775a3894ed31116b4f568
peter bohac <pbohac@apple.com>
2019-12-26 17:11:52 -0800

    Refactored saved states into checkpoints and added support for saving/loading game state

--- SynacorVM/main.swift
+++ SynacorVM/main.swift
@@ -8,42 +8,45 @@
 
 import Foundation
 
-var program: [Int]!
-var saveFileUrl: URL? = nil
-
-struct SavedState: Codable {
-    var name: String
-    var state: SynacorVM.State
+var programFilename: String?
+var checkpointsFilename: String?
+var savedStateFilename: String?
+var unusedArgs: [String] = []
+var arguments = CommandLine.arguments.dropFirst()
+while arguments.isEmpty == false {
+    let arg = arguments.removeFirst()
+    if (arg == "--program" || arg == "-p") && arguments.count > 0 {
+        programFilename = arguments.removeFirst()
+    } else if (arg == "--checkpoints" || arg == "-c") && arguments.count > 0 {
+        checkpointsFilename = arguments.removeFirst()
+    } else if (arg == "--load" || arg == "-l") && arguments.count > 0 {
+        savedStateFilename = arguments.removeFirst()
+    } else {
+        if programFilename == nil {
+            programFilename = arg
+        } else if checkpointsFilename == nil {
+            checkpointsFilename = arg
+        } else if savedStateFilename == nil {
+            savedStateFilename = arg
+        } else {
+            unusedArgs.append(arg)
+        }
+    }
 }
 
-var savedStates: [SavedState?] = Array(repeating: nil, count: 5)
+guard unusedArgs.isEmpty, let programFilename = programFilename else {
+    print("Usage: [--program] <program filename> [[--checkpoints] <checkpoints filename>] [[--load] <saved state filename>]")
+    exit(0)
+}
 
-func loadProgram(file: String) -> [Int] {
-    let pwd = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
-    let inputFileUrl = pwd.appendingPathComponent(file)
+let pwd = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+let program: [Int] = {
+    let inputFileUrl = pwd.appendingPathComponent(programFilename)
     let data = try! Data(contentsOf: inputFileUrl)
     var temp: [UInt16] = Array(repeating: 0, count: data.count / 2)
     _ = temp.withUnsafeMutableBytes { data.copyBytes(to: $0) }
     return temp.map { Int($0) }
-}
-
-if CommandLine.arguments.count == 3 && CommandLine.arguments[1] == "--raw" {
-    let raw = CommandLine.arguments[2]
-    program = raw.components(separatedBy: ",").map { Int($0.trimmingCharacters(in: .whitespaces))! }
-} else if CommandLine.arguments.count == 3 {
-    program = loadProgram(file: CommandLine.arguments[1])
-    let pwd = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
-    saveFileUrl = pwd.appendingPathComponent(CommandLine.arguments[2])
-    let decoder = PropertyListDecoder()
-    if let data = try? Data(contentsOf: saveFileUrl!), let saved = try? decoder.decode([SavedState?].self, from: data) {
-        savedStates = saved
-    }
-} else if CommandLine.arguments.count == 2 {
-    program = loadProgram(file: CommandLine.arguments[1])
-} else {
-    print("Usage: <binary file> [<save file>] | --raw <comma separated integers>")
-    exit(0)
-}
+}()
 
 var inputBuffer: [Character] = []
 var outputBuffer: String = ""
@@ -61,30 +64,69 @@ let vm = SynacorVM(program: program, input: { return inputBuffer.popLast() }) { 
     return true
 }
 
-var encoder = PropertyListEncoder()
+struct Checkpoint: Codable {
+    var name: String
+    var state: SynacorVM.State
+}
 
-func save(state: SynacorVM.State, name: String) {
-    for idx in (1 ..< savedStates.count).reversed() {
-        savedStates[idx] = savedStates[idx - 1]
+let decoder = PropertyListDecoder()
+var encoder = PropertyListEncoder()
+var checkpointsFileUrl: URL? = nil
+var checkpoints: [Checkpoint?] = Array(repeating: nil, count: 5)
+
+func saveCheckpoint(state: SynacorVM.State, name: String) {
+    for idx in (1 ..< checkpoints.count).reversed() {
+        checkpoints[idx] = checkpoints[idx - 1]
     }
-    savedStates[0] = SavedState(name: name, state: state)
-    if let saveFileUrl = saveFileUrl {
-        let data = try! encoder.encode(savedStates)
+    checkpoints[0] = Checkpoint(name: name, state: state)
+    if let saveFileUrl = checkpointsFileUrl {
+        let data = try! encoder.encode(checkpoints)
         try! data.write(to: saveFileUrl)
     }
 }
 
-func restore(index: Int) {
-    guard let state = savedStates[index]?.state else {
+func restoreCheckpoint(index: Int) {
+    guard let state = checkpoints[index]?.state else {
         preconditionFailure()
     }
     vm.state = state
     inputBuffer = "look\n".map(Character.init).reversed()
 }
 
+func saveGameState(to filename: String) {
+    let fileUrl = pwd.appendingPathComponent(filename)
+    if let data = try? encoder.encode(vm.state), (try? data.write(to: fileUrl)) != nil {
+        print("\nSaved to \(filename)\n")
+    }
+}
+
+func loadGameState(from filename: String) {
+    let fileUrl = pwd.appendingPathComponent(filename)
+    if let data = try? Data(contentsOf: fileUrl), let state = try? decoder.decode(SynacorVM.State.self, from: data) {
+        vm.state = state
+        inputBuffer = "look\n".map(Character.init).reversed()
+        print("\nLoaded saved state from \(filename)\n")
+    }
+}
+
+if let filename = checkpointsFilename {
+    checkpointsFileUrl = pwd.appendingPathComponent(filename)
+    if let data = try? Data(contentsOf: checkpointsFileUrl!), let savedCheckpoints = try? decoder.decode([Checkpoint?].self, from: data) {
+        checkpoints = savedCheckpoints
+    }
+}
+
+if let filename = savedStateFilename {
+    loadGameState(from: filename)
+} else if checkpoints[0] != nil {
+    restoreCheckpoint(index: 0)
+}
+
 enum Input: Equatable {
     case quit
     case restore(Int)
+    case save(String)
+    case load(String)
     case other(String)
 }
 
@@ -98,21 +140,33 @@ func getInput(prompt: String? = nil) -> Input {
     }
     if line.hasPrefix("restore") {
         let idx = Int(line.components(separatedBy: " ").last!)
-        if let idx = idx, idx >= 0, idx < savedStates.count, savedStates[idx] != nil {
+        if let idx = idx, idx >= 0, idx < checkpoints.count, checkpoints[idx] != nil {
             return .restore(idx)
         }
-        savedStates.enumerated().forEach { idx, state in
+        checkpoints.enumerated().forEach { idx, state in
             if let state = state {
                 print("\(idx) - \(state.name)")
             }
         }
         return getInput(prompt: prompt)
     }
+    if line.hasPrefix("save") {
+        let file = String(line.dropFirst(5))
+        if file.isEmpty {
+            print("Usage: save <filename>")
+            return getInput(prompt: prompt)
+        }
+        return .save(file)
+    }
+    if line.hasPrefix("load") {
+        let file = String(line.dropFirst(5))
+        if file.isEmpty {
+            print("Usage: load <filename>")
+            return getInput(prompt: prompt)
+        }
+        return .load(file)
+    }
     return .other(line)
-}
-
-if savedStates[0] != nil {
-    restore(index: 0)
 }
 
 while true {
@@ -127,20 +181,24 @@ while true {
                 exit(0)
             } else if case .restore(let idx) = input {
                 needsResponse = false
-                restore(index: idx)
+                restoreCheckpoint(index: idx)
             }
         } while needsResponse
 
     case .inputNeeded:
         let input = getInput()
         if input == .quit {
-            save(state: vm.state, name: currentRoom)
+            saveCheckpoint(state: vm.state, name: currentRoom)
             exit(0)
         } else if case .restore(let idx) = input {
-            restore(index: idx)
+            restoreCheckpoint(index: idx)
+        } else if case .save(let filename) = input {
+            saveGameState(to: filename)
+        } else if case .load(let filename) = input {
+            loadGameState(from: filename)
         } else if case .other(let line) = input {
             if !(line == "help" || line == "inv" || line.hasPrefix("look")) {
-                save(state: vm.state, name: currentRoom)
+                saveCheckpoint(state: vm.state, name: currentRoom)
             }
             inputBuffer = (line + "\n").map(Character.init).reversed()
         } else {
